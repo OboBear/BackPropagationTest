2816ea3f8a92cba15528b2ef60881ce46c8f941a
obo <obo.lin@dingtone.me>
2017-08-31 00:44:31 +0800

    Add log

--- Main.py
+++ Main.py
@@ -10,14 +10,18 @@ LEARNING_RATE = 0.5
 def logistic(x):
 	return 1.0 / (1 + np.exp(-x))
 
+def log(output):
+	1+1
+	# print(output)
+
 # 神经网络
 class NeuronNetwork(object):
 	def __init__(self, inputSize, net1Size, net2Size):
-		print("\n=============================")
-		print("Init")
-		print("=============================")
+		log("\n=============================")
+		log("Init")
+		log("=============================")
 
-		print("NeuronNetwork inputSize = %d net1Size = %d net2Size = %d" %(inputSize, net1Size, net2Size))
+		log("NeuronNetwork inputSize = %d net1Size = %d net2Size = %d" %(inputSize, net1Size, net2Size))
 		self.inputSize = inputSize
 		self.net1Size = net1Size
 		self.net2Size = net2Size
@@ -30,56 +34,68 @@ class NeuronNetwork(object):
 		return np.array(array)
 
 	def train(self, input, targetOutput):
-		print("\n=============================")
-		print("Train")
-		print("=============================")
+		log("\n=============================")
+		log("Train")
+		log("=============================")
 		# 添加偏置 1
-		input = self.appendOne(input)
-		net1Output = self.net1Layer.calculate(input);
+		net1Input = self.appendOne(input)
+		net1Output = self.net1Layer.calculate(net1Input);
 		net1Output = np.array(net1Output)
 
 		# 添加偏置 1
 		net1Output = self.appendOne(net1Output)
-		print("=============================")
-		print("Calculate net1Output")
-		print(net1Output)
+		log("=============================")
+		log("Calculate net1Output")
+		log(net1Output)
 		net2Output = self.net2Layer.calculate(net1Output);
 		net2Output = np.array(net2Output)
-		print("=============================")
-		print("Calculate TotalOutput")
-		print(net2Output)
-		print("=============================")
-		print("Calculate distance")
-		differ = targetOutput - net2Output
-		print(differ * differ)
-		print("temp1")
-		temp1 = differ * net2Output * (1 - net2Output)
-		temp1.shape = (len(temp1), 1)
-		print(temp1)
-		net1Output.shape = (1, len(net1Output))
-		thetaOutput = np.dot(temp1, net1Output)
-		print("=============================")
-		print("thetaOutput")
-		print(thetaOutput)
+		log("=============================")
+		log("Calculate TotalOutput")
+		log(net2Output)
 
-		# temp1
-		weight = self.net2Layer.getWeight()
-		# print("weight")
+		# 开始反向传播调整权值
+		log("=============================")
+		log("Calculate distance")
+		outputDiffer = net2Output - targetOutput
+		log(outputDiffer * outputDiffer)
+		# print(outputDiffer)
+		log("temp1")
+		temp1 = outputDiffer * net2Output * (1 - net2Output)
+		temp1.shape = (len(temp1), 1)
+		log(temp1)
+		net1Output.shape = (1, len(net1Output))
+		# 生成第二层的梯度
+		thetaOutput = np.dot(temp1, net1Output)
+		log("=============================")
+		log("thetaOutput")
+		log(thetaOutput)
+		log("=============================")
+
+		net2Weight = self.net2Layer.getWeight()
+		log("net2Weight")
+		log(net2Weight)
 
 		temp1.shape = (1, len(temp1))
-		thetaHideStep1 = np.dot(temp1,weight)[0] * input
+		print(temp1)
+		print(net2Weight)
+		print(np.dot(temp1,net2Weight))
+		print(net1Input)
+		net1InputMatrix = np.array([net1Input])
+		thetaHideStep1 = np.dot(np.dot(temp1,net2Weight).T , net1InputMatrix)
+		print(thetaHideStep1)
 		thetaHideStep1 = np.split(thetaHideStep1,[self.net1Size])[0]
+		log(thetaHideStep1)
 		print(thetaHideStep1)
-		thetaHideStep1.shape = [len(thetaHideStep1), 1]
-		thetaHideStep1 = np.array(thetaHideStep1)
+		# thetaHideStep1.shape = [len(thetaHideStep1), 1]
+		# thetaHideStep1 = np.array(thetaHideStep1)
 		newInput = np.array([input])
-		print("newInput")
-		print(newInput)
-		print("=============================")
-		print("thetaHideStep1")
-		print(thetaHideStep1)
+		log("newInput")
+		log(newInput)
+		log("=============================")
+		log("thetaHideStep1")
+		log(thetaHideStep1)
 		thetaHide = np.dot(thetaHideStep1, newInput)
-		print(thetaHide)
+		log(thetaHide)
 
 		self.net2Layer.updateWeight(thetaOutput)
 		self.net1Layer.updateWeight(thetaHide)
@@ -98,17 +114,17 @@ class NeuronLayer(object):
 		self.neuronLayer = []
 		for x in range(0,layerSize):
 			self.neuronLayer.append(Neuron(inputSize = inputSize + 1))
-		print("NeuronLayer inputSize = %d bias = %d" %(inputSize, 1))
+		log("NeuronLayer inputSize = %d bias = %d" %(inputSize, 1))
 
 	def calculate(self, input):
 		result = []
-		print("self.neuronLayer")
-		print(len(self.neuronLayer))
+		log("self.neuronLayer")
+		log(len(self.neuronLayer))
 		for x in range(0, len(self.neuronLayer)):
 			result.append(self.neuronLayer[x].calculate(input));
-		print("result")
-		print(result)
-		print(np.array(result))
+		log("result")
+		log(result)
+		log(np.array(result))
 		return result
 	def updateWeight(self, theta):
 		for x in xrange(0,len(theta)):
@@ -122,7 +138,7 @@ class NeuronLayer(object):
 # 神经节点
 class Neuron(object):
 	def __init__(self, inputSize, weight = None):
-		print("int Neuron")
+		log("int Neuron")
 		self.inputSize = inputSize
 		if weight is not None:
 			self.inputWight = weight
@@ -130,37 +146,39 @@ class Neuron(object):
 			self.inputWight = np.ones(inputSize)
 	
 	def calculate(self, inputData):
-		print("inputData")
-		print(inputData)
-		print(self.inputWight)
+		log("inputData")
+		log(inputData)
+		log(self.inputWight)
 		self.netOut = np.sum(inputData * self.inputWight)
 		self.netOut = logistic(self.netOut)
 		return self.netOut
 
 	def updateWeight(self, thetaX):
-		self.inputWight = self.inputWight + LEARNING_RATE * thetaX;
+		self.inputWight = self.inputWight - LEARNING_RATE * thetaX;
 
 	def getWeight(self):
 		return self.inputWight
 
-neuronNetwork = NeuronNetwork(inputSize = 2, net1Size = 2,net2Size = 2)
+neuronNetwork = NeuronNetwork(inputSize = 2, net1Size = 6,net2Size = 1)
 # for x in xrange(1,1000):
 # 	neuronNetwork.train(input = np.array([1,0]), targetOutput = np.array([1, 2]))	
-for x in xrange(0,100):
-	neuronNetwork.train(input = np.array([0.05,0.10]), targetOutput = np.array([0.01, 0.99]))
-	neuronNetwork.train(input = np.array([0.3,0.70]), targetOutput = np.array([0.90, 0.01]))
+for x in xrange(0,1000):
+	neuronNetwork.train(input = np.array([1,0]), targetOutput = np.array([0]))
+	neuronNetwork.train(input = np.array([0,1]), targetOutput = np.array([0]))
+	neuronNetwork.train(input = np.array([1,1]), targetOutput = np.array([1]))
+	neuronNetwork.train(input = np.array([0,0]), targetOutput = np.array([1]))
 # neuronNetwork.train(input = np.array([1,0]), targetOutput = np.array([1, 2]))
 
-print("Train result")
+log("Train result")
 outputFinal = neuronNetwork.calculate(input = np.array([0.05,0.10]))
-print("outputFinal1")
-print(outputFinal)
+log("outputFinal1")
+log(outputFinal)
 
 outputFinal = neuronNetwork.calculate(input = np.array([0.3,0.70]))
-print("outputFinal2")
-print(outputFinal)
+log("outputFinal2")
+log(outputFinal)
 
-print("Train End")
+log("Train End")
 
 # [0,0],[0]
 # [0,1],[1]
